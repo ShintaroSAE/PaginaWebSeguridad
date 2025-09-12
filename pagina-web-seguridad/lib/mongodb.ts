ed5a5c5778bcf84be4e8e3f30ebf4b2265323b8e
ShintaroSAE <salagued96@gmail.com>
2025-09-11 18:24:48 -0600

    implementacion de MongoDB

---
+++ pagina-web-seguridad/lib/mongodb.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // @ts-ignore
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    // @ts-ignore
+    global._mongoClientPromise = client.connect();
+  }
+  // @ts-ignore
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
